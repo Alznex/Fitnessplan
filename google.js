1a9a8b0debb99f0c352f2232581a763bc92c009e
Alznex <92757663+Alznex@users.noreply.github.com>
2023-07-04 18:15:31 +0200

    modified:   google.js

--- google.js
+++ google.js
@@ -1,3 +1,8 @@
 function login(){
-    console.log(credential)
+    let ID = responsePayload.sub;
+    let name = responsePayload.name;
+    let alias = responsePayload.given_name;
+    let familien_name = responsePayload.family_name;
+    let img = responsePayload.picture;
+    let email = responsePayload.email;
 }
