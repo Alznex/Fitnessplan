7e7ddcd9d517c48378e4423cae8a5e5c059fb381
Alznex <92757663+Alznex@users.noreply.github.com>
2023-07-04 20:56:36 +0200

    modified:   google.js 	modified:   index.html

--- google.js
+++ google.js
@@ -1,3 +1,54 @@
+ // Discovery doc URL for APIs used by the quickstart
+ const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+ let gapiInited = false;
+
+function gapiLoaded() {
+    gapi.load('client', initializeGapiClient);
+  }
+
+  /**
+   * Callback after the API client is loaded. Loads the
+   * discovery doc to initialize the API.
+   */
+  async function initializeGapiClient() {
+    let API_KEY = "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"
+    let client_id = "992099953263-kfvfhm63aige76ilpjlp2upt62cfigg6.apps.googleusercontent.com"
+
+    await gapi.client.init({
+      apiKey: API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
+  }
+
+ /**
+       * Print metadata for first 10 files.
+       */
+ async function listFiles() {
+    let response;
+    try {
+      response = await gapi.client.drive.files.list({
+        'pageSize': 10,
+        'fields': 'files(id, name)',
+      });
+    } catch (err) {
+      document.getElementById('main').innerText = err.message;
+      return;
+    }
+    const files = response.result.files;
+    if (!files || files.length == 0) {
+      document.getElementById('main').innerText = 'No files found.';
+      return;
+    }
+    // Flatten to string to display
+    const output = files.reduce(
+        (str, file) => `${str}${file.name} (${file.id})\n`,
+        'Files:\n');
+    document.getElementById('main').innerText = output;
+  }
+
+
 function decodeJwtResponse(token) {
     var base64Url = token.split(".")[1];
     var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
@@ -17,10 +68,8 @@ function handleCredentialResponse(response){
     const responsePayload = decodeJwtResponse(response.credential);
 
     let ID = responsePayload.sub;
-    let name = responsePayload.name;
-    let alias = responsePayload.given_name;
-    let familien_name = responsePayload.family_name;
-    let img = responsePayload.picture;
-    let email = responsePayload.email;
-    console.log("ID:" & ID)
+    localStorage.setItem("user_id", ID);
+}
+
+function saveToGoogle(){
 }
